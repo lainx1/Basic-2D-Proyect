49e49f8d6545f2cb76ca8faa5968ba839f147ae7
lainx1 <ehecattl@hotmail.com>
2019-07-15 22:52:22 -0500

    Added: Leaf controller

---
+++ Assets/Scripts/BackArrow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackArrow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
